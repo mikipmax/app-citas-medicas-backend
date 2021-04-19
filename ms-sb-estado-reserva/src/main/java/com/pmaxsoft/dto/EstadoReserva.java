1d72063ee9d7a1d9418e6731a8d75a72ada8d87f
mikipmax <pmax-111@hotmail.es>
2021-04-18 22:29:22 -0500

    comentado

--- ms-sb-estado-reserva/src/main/java/com/pmaxsoft/dto/EstadoReserva.java
+++ ms-sb-estado-reserva/src/main/java/com/pmaxsoft/dto/EstadoReserva.java
@@ -1,5 +1,21 @@
 package com.pmaxsoft.dto;
+/*
+***UNIVERSIDAD CENTRAL DEL ECUADOR***
+***FACULTAD DE INGENIERIA Y CIENCIAS APLICADAS***
+***CARRERAS: INGENIERÍA EN COMPUTACIÓN GRÁFICA​E INGENIERÍA INFORMÁTICA***
+***MATERIA: DISPOSITIVOS MÓVILES***
 
+Grupo N: 5
+Integrantes:  -Nicolalde Estefanía ​Correo: jenicolaldep@uce.edu.ec
+              -Ponce Michael​​Correo: mfponce@uce.edu.ec
+              -Sánchez Jonathan​​Correo: jjsanchezl1@uce.edu.ec
+              -Tituaña Mayra​Correo: mrtituana@uce.edu.ec
+Descripción: La aplicación DATAMED se manifiesta de forma similar a una agenda, y tiene el propósito
+*            de agilizar el proceso de consulta. Consume microservicios alojados en un servidor de
+*            cloud gratuito, permite a los administrativos registrar las citas médicas de
+*            los pacientes brindando la facilidad de gestionar parámetros como: la fecha,
+*            especialista, área y tipo de pago.
+*/
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -7,6 +23,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.Data;
+
 
 @Entity
 @Data
--- ms-sb-reservacion/src/main/java/com/pmaxsoft/repositorios/RepositorioReservacion.java
+++ ms-sb-reservacion/src/main/java/com/pmaxsoft/repositorios/RepositorioReservacion.java
@@ -1,11 +1,27 @@
 package com.pmaxsoft.repositorios;
+/*
+***UNIVERSIDAD CENTRAL DEL ECUADOR***
+***FACULTAD DE INGENIERIA Y CIENCIAS APLICADAS***
+***CARRERAS: INGENIERÍA EN COMPUTACIÓN GRÁFICA​E INGENIERÍA INFORMÁTICA***
+***MATERIA: DISPOSITIVOS MÓVILES***
 
+Grupo N: 5
+Integrantes:  -Nicolalde Estefanía ​Correo: jenicolaldep@uce.edu.ec
+              -Ponce Michael​​Correo: mfponce@uce.edu.ec
+              -Sánchez Jonathan​​Correo: jjsanchezl1@uce.edu.ec
+              -Tituaña Mayra​Correo: mrtituana@uce.edu.ec
+Descripción: La aplicación DATAMED se manifiesta de forma similar a una agenda, y tiene el propósito
+*            de agilizar el proceso de consulta. Consume microservicios alojados en un servidor de
+*            cloud gratuito, permite a los administrativos registrar las citas médicas de
+*            los pacientes brindando la facilidad de gestionar parámetros como: la fecha,
+*            especialista, área y tipo de pago.
+*/
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 
 import com.pmaxsoft.dto.Reservacion;
-
+//Esta interface la implementa spring boot, permite generar un Crud básico
 
 @Repository
 public interface RepositorioReservacion extends CrudRepository<Reservacion, Integer> {
